2a725bf222a81e09127468b756c4eb0f0e28f5a7
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 06:38:01 +0500

    Update gp-invite.js

--- plugins/gp-invite.js
+++ plugins/gp-invite.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) throw `✳️ Ingrese el número al que quiere enviar una invitación al grupo\n\n📌 Ejemplo :\n*${usedPrefix + command}* 59172945992`
-if (text.includes('+')) throw  `✳️ Ingrese el número todo junto sin el *+*`
-if (isNaN(text)) throw ' 📌 Ingrese sólo números más su código de país sin espacios'
+if (!text) throw `✳️ Введите номер, на который вы хотите отправить приглашение в группу\n\n📌 Пример :\n*${usedPrefix + command}* 59172945992`
+if (text.includes('+')) throw  `✳️ Введите число все вместе без *+*`
+if (isNaN(text)) throw ' 📌 Вводите только цифры плюс код вашей страны без пробелов'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
  
       await conn.reply(text+'@s.whatsapp.net', `≡ *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
-        m.reply(`✅ Se envió un enlace de invitación al usuario`) 
+        m.reply(`✅ Пользователю была отправлена ссылка для приглашения`) 
 
 }
 handler.help = ['invite <549xxx>']
