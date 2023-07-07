5c69eb847c5e98d528a6c91772e73caa7f8078d1
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-08 04:39:34 +0500

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,14 +1,28 @@
+const { sticker5 } = require('../lib/sticker')
+const fetch = require('node-fetch')
 
-import { sticker } from '../lib/sticker.js'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-     if (!text) throw `✳️ _послать текст_\n\n📌Пример *${usedPrefix + command}* dylux-fg`
-    //let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text }), global.packname, global.author)
-     let stiker = await sticker(null, global.API('fgmods', '/api/maker/attp', { text }, 'apikey'), global.packname, global.author)
-    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m )
+let handler = async (m, { conn, text, command }) => {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+
+  if (/^attp1?$/i.test(command)) {
+    let res = await fetch(global.API('xteam', '/attp', { file: '', text: teks }))
+    if (!res.ok) throw eror
+    conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  }
+
+  if (/2$/i.test(command)) {
+    let url = await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2gif/', { text: teks }))
+    if (!url.ok) throw eror
+    res = await url.json()
+    let stiker = await sticker5(null, res.image, global.packname, global.author)
+    if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
     throw stiker.toString()
+  }
 }
-handler.help = ['attp <text>']
+handler.help = new Array(2).fill('attp').map((v, i) => v + (i + 1) + ' <teks>')
 handler.tags = ['sticker']
-handler.command = ['attp'] 
 
-export default handler
+handler.command = /^attp[1-2]?$/i
+handler.limit = true
+
+module.exports = handler
