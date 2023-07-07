6939f935bacff128a066b68bd3344f4b83b867bc
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 06:19:57 +0500

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @user`
+        if (!who) throw `✳️ Пометьте или упомяните кого-нибудь\n\n📌 Ejemplo : ${usedPrefix + command} @user`
         if (!(who in global.db.data.users)) throw `✳️ Пользователя нет в моей базе данных`
         let name = conn.getName(m.sender)
         let warn = global.db.data.users[who].warn
