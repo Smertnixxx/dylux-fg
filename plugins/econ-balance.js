caaca7556c1bfd800925528b1af16fe7644926cf
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:40:42 +0500

    Update econ-balance.js

--- plugins/econ-balance.js
+++ plugins/econ-balance.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn, usedPrefix}) => {
 	
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
-    if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+    if (!(who in global.db.data.users)) throw `✳️ Пользователя нет в моей базе данных`
     conn.reply(m.chat, `
-┌───⊷ *BALANCE* ⊶
-▢ *📌Nombre* : _@${who.split('@')[0]}_
-▢ *💎Diamantes* : _${user.diamond}_
+┌───⊷ *Баланс* ⊶
+▢ *📌Имя* : _@${who.split('@')[0]}_
+▢ *💎Алмазы* : _${user.diamond}_
 ▢ *⬆️XP* : _Total ${user.exp}_
 └──────────────
 
-*NOTA :* 
-Puedes comprar 💎 diamantes usando los comandos
+*ПРИМЕЧАНИЕ :* 
+Вы можете купить 💎 алмазы, используя команды
 ❏ *${usedPrefix}buy <cantidad>*
 ❏ *${usedPrefix}buyall*`, m, { mentions: [who] })
 }
