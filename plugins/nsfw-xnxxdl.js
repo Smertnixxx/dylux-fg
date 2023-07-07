0f284a76c64773a3446655e8c96df7390669e227
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 21:44:27 +0500

    Update nsfw-xnxxdl.js

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -4,26 +4,26 @@ import fg from 'api-dylux'
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
  let chat = global.db.data.chats[m.chat]
-  if (!chat.nsfw) throw `🚫 El grupo no admite contenido nsfw \n\nPara habilitar escriba \n*${usedPrefix}enable* nsfw`
+  if (!chat.nsfw) throw `🚫 Группа не поддерживает контент nsfw \n\nчтобы включить \n*${usedPrefix}enable* nsfw`
   let user = global.db.data.users[m.sender].age
-  if (user < 17) throw `❎ Eres menor de edad! vuelve cuando tengas más de 18 años`
-  if (!text) throw `✳️ Para buscar\n📌 Use : *${usedPrefix + command} <search>*\n\nPara descargar desde URL:\n📌Use : *${usedPrefix + command} <url>*`
+  if (user < 17) throw `❎ возвращайся, когда тебе будет больше 18 лет`
+  if (!text) throw `✳️ Для поиска\n📌 Используй : *${usedPrefix + command} <search>*\n\nДля загрузки URL:\n📌Use : *${usedPrefix + command} <url>*`
     
     m.react(rwait)
     if (text.includes('http://') || text.includes('https://')) {
-        if (!text.includes('xnxx.com')) return m.reply(`❎ Ingrese un link de *xnxx.com*`)
+        if (!text.includes('xnxx.com')) return m.reply(`❎ Введите ссылку из *xnxx.com*`)
         try {
             let xn = await fg.xnxxdl(text)
             conn.sendFile(m.chat, xn.result.files.high, xn.result.title + '.mp4', `
 ≡  *XNXX DL*
             
-▢ *📌Título*: ${xn.result.title}
-▢ *⌚Duración:* ${xn.result.duration}
-▢ *🎞️Calidad:* ${xn.result.quality}
+▢ *📌Заголовок*: ${xn.result.title}
+▢ *⌚Продолжительность:* ${xn.result.duration}
+▢ *🎞️Качество:* ${xn.result.quality}
 `.trim(), m, false, { asDocument: chat.useDocument })
  m.react(done)
  } catch (e) {
-    m.reply(`🔴 Error : intenta mas tarde`)
+    m.reply(`🔴 Ошибка : попробуйте позже`)
  }
     } else {
         try {
@@ -31,7 +31,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
             let ff = res.result.map((v, i) => `${i + 1}┃ *Titulo* : ${v.title}\n*Link:* ${v.link}\n`).join('\n') 
               if (res.status) m.reply(ff)
             } catch (e) {
-              m.reply(`🔴 Error: intenta mas tarde`)
+              m.reply(`🔴 Ошибка : попробуйте позже`)
                }
     }
 }
