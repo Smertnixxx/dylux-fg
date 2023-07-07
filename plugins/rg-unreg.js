1d91e1a3403db6da6f6fb14f12a7b021de6356b0
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:47:15 +0500

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Ingrese número de serie*\nVerifique su número de serie con el comando...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `✳️ *Введите серийный номер*\nПроверьте свой серийный номер с помощью команды...\n\n*${usedPrefix}nserie*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw '⚠️ *Número de serie incorrecto*'
   user.registered = false
-  m.reply(`✅ Registro eliminado`)
+  m.reply(`✅ Запись удалена`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
