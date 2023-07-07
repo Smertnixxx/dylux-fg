010f850d82417d7a282e007dda3fa5063a15d870
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 09:08:57 +0500

    Update owner-checkexpired.js

--- plugins/owner-checkexpired.js
+++ plugins/owner-checkexpired.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	
-	if (global.db.data.chats[m.chat].expired < 1) throw `✳️ Este grupo no está configurado para caducar`
+	if (global.db.data.chats[m.chat].expired < 1) throw `✳️ Срок действия этой группы не установлен`
     let who
     if (m.isGroup) who = args[1] ? args[1] : m.chat
     else who = args[1]
@@ -9,11 +9,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     var nDays = 86400000 * args[0]
     var now = new Date() * 1
     
-    m.reply(`✳️ Su alquiler expira en 
+    m.reply(`✳️ Срок вашей аренды истекает в
     
     ${msToDate(global.db.data.chats[who].expired - now)}
 
-  _Despues el bot saldrá automáticamente del grupo_`) 
+  _после этого бот автоматически выйдет из группы_`) 
     
 }
 handler.help = ['checkexpired']
