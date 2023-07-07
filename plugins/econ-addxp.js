97bf0cbe16e8e81b390262628a6b3e8f83f37262
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:42:08 +0500

    Update econ-addxp.js

--- plugins/econ-addxp.js
+++ plugins/econ-addxp.js
@@ -4,20 +4,20 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw '✳️ Taguea al usuario'
+  if (!who) throw '✳️ Помечает пользователя'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (!txt) throw '✳️ Ingrese la cantidad de *XP* que quiere añadir'
+  if (!txt) throw '✳️ Введите количество *XP*, которое вы хотите добавить'
   if (isNaN(txt)) throw ' 🔢 sólo números'
   let xp = parseInt(txt)
   let exp = xp
   
-  if (exp < 1) throw '✳️ Mínimo es  *1*'
+  if (exp < 1) throw '✳️ Минимум равен *1*'
   let users = global.db.data.users
   users[who].exp += xp
 
-  await m.reply(`≡ *XP AÑADIDO*
+  await m.reply(`≡ *ДОБАВЛЕН XP*
 ┌──────────────
-▢  *Total:* ${xp}
+▢  *Текущий:* ${xp}
 └──────────────`)
  conn.fakeReply(m.chat, `▢ Recibiste \n\n *+${xp} XP*`, who, m.text)
 }
