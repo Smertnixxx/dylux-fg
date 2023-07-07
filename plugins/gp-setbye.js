57e35c853c2b890743b2b649cc82eae6c8d32bd7
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 06:27:23 +0500

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
-    m.reply('✅ Se estableció el mensaje de despedida')
-  } else throw `✳️ Ingrese el mensaje\n@user (mención)`
+    m.reply('✅ Было установлено прощальное послание')
+  } else throw `✳️ Выберете сообщение \n@user (Упомнинание)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
