b9eaccc76c7d5d42c3fa6fd6c903e3de72e15580
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:19:28 +0500

    Update anime-rpat.js

--- plugins/anime-rpat.js
+++ plugins/anime-rpat.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
    let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+     if (!who) throw `✳️ Пометьте или упомяните кого-нибудь\n\n📌 Пример : ${usedPrefix + command} @tag` 
      
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -18,7 +18,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rpat.ok) throw await rpat.text()
    let json = await rpat.json()
    let { url } = json
-   let stiker = await sticker(null, url, `(${name2}) acarició a`, `${name}`)
+   let stiker = await sticker(null, url, `(${name2}) он погладил`, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('☺️') 
    
