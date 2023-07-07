44f7528b52a113afe8d5bbccaf459b5e11ef97e6
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:21:06 +0500

    Update anime-rkill.js

--- plugins/anime-rkill.js
+++ plugins/anime-rkill.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+    if (!who) throw `✳️ Пометьте или упомяните кого-нибудь\n\n📌 Пример : ${usedPrefix + command} @tag` 
     
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) mató a`, `${name}`)
+   let stiker = await sticker(null, url, `(${name2}) он убил`, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('🗡️') 
    
