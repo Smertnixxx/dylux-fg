54cb1f35f3e8dee6ad0988bc660f73b49dff0aca
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:33:26 +0500

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
 import fg from 'api-dylux';
 let handler = async (m, { conn, command, args, text }) => {
       
-    if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
+    if (!args[0]) return m.reply('✳️ Введите URL-адрес страницы')
     m.react(rwait) 
 	let full = /f$/i.test(command)
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
     //let ss = await fg.ssweb(url, /f$/i.test(command), args[1])
-    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m) 
+    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Захват страницы', m) 
    m.react(done) 
 }
 handler.help = ['ssweb <url>']
