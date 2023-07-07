8635af4b62053a9dc541d1c1954370a6c7e7fff6
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 09:16:33 +0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🟢 *Velocidad* : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`${ssd}🟢 *Скорость* : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
