8fca77bbd3debc19c56cccc76560c38978225d98
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:27:46 +0500

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,10 +11,10 @@ export async function before(m, { conn }) {
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
-*▢ SUBISTE DE NIVEL*
+*▢ ТЫ ПОВЫСИЛ СВОЙ УРОВЕНЬ*
 
  *${before}* ‣  *${user.level}*
- Rango : *${user.role}*
+ Ранг : *${user.role}*
  
  _para desactivar escribe_
 _*/off autolevelup*_
