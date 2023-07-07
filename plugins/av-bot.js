666eb28f3bb6efc1335f5265dc07b34c678291a9
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 23:52:23 +0500

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn}) => {
 let name = conn.getName(m.sender)
 let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
 
-conn.sendButton(m.chat, `Hola *${name}* \n \nВам нужна помощь? \n`, fgig, null, [
+conn.sendButton(m.chat, `Привет! *${name}* \n \nВам нужна помощь? \n`, fgig, null, [
       ['⦙☰ Меню', '/help'],
       ['⦙☰ Меню 2', '/menu2'],
       ['⌬ Группы', '/gpdylux']
