30e060c1596d26d768a255d783922215b44534bf
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:10:59 +0500

    Update dl-gdrive.js

--- plugins/dl-gdrive.js
+++ plugins/dl-gdrive.js
@@ -2,15 +2,15 @@
 import fg from 'api-dylux'  
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Ingrese un link de Google Drive`
+	if (!args[0]) throw `✳️ Введите ссылку на Google Диск`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-≡ *Google Drive DL*
-▢ *Nombre:* ${res.fileName}
-▢ *Tamaño:* ${res.fileSize}
-▢ *tipo:* ${res.mimetype}`)
+≡ *Гугл диск*
+▢ *Имя:* ${res.fileName}
+▢ *Размер:* ${res.fileSize}
+▢ *Тип:* ${res.mimetype}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
