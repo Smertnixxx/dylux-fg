aa0d1fd124c51482f727b052637925c3f1ee9f44
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-08 00:34:00 +0500

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ Деактивирован ли бот в этой группе')
+    m.reply('✅ Бот *деактивирован* в этой группе')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
