4b536be90e1d6ae74d6937c26bc2b4b049a5cdcc
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:16:20 +0500

    Update fun-preg.js

--- plugins/fun-preg.js
+++ plugins/fun-preg.js
@@ -3,16 +3,16 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	
  let name = conn.getName(m.sender)
-  if (!text) throw `✳️ *Ejemplo :*\n\n *${usedPrefix + command}* soy feo?`
+  if (!text) throw `✳️ *Пример :*\n\n *${usedPrefix + command}* я уродлив?`
   m.react('🫣') 
   //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
   let json = await res.json()
   if (json.success) 
-m.reply(`≡ *PREGUNTAS*
+m.reply(`≡ *Расспросы*
  
-▢ *Pregunta:* ${text}
-▢ *Respuesta :* ${json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux')}`) 
+▢ *Спрашивай:* ${text}
+▢ *Ответ :* ${json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux')}`) 
   else throw json
 }
 
