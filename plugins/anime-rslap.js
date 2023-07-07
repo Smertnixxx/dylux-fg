46f740450e1f0224db63a17764aa8878e0b5fb5d
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 08:18:37 +0500

    Update anime-rslap.js

--- plugins/anime-rslap.js
+++ plugins/anime-rslap.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag`
+     if (!who) throw `✳️ Пометьте или упомяните кого-нибудь\n\n📌 Пример : ${usedPrefix + command} @tag`
      
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -18,7 +18,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) le dio una bofetada a`, `${name}`)
+   let stiker = await sticker(null, url, `(${name2}) он дал пощечину`, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('👊🏻') 
    
