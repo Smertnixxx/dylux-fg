bda792b4f975c27705cbe02c9dd8ce2637e31523
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:11:53 +0500

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,9 +1,9 @@
 
 let handler = async (m, { conn, text }) => {
 	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`✳️ No estas en partida de TicTacToe 🎮 `, m)
+if (room == undefined) return conn.reply(m.chat,`✳️ Ты не в игре в Тиктакто 🎮 `, m)
 delete conn.game[room.id]
-await conn.reply(m.chat, `✅ Se reinició la sesión de *tictactoe 🎮*`, m)
+await conn.reply(m.chat, `✅ Сеанс *tictactoe был перезапущен 🎮*`, m)
 }
 handler.help = ['delttt']
 handler.tags = ['game']
