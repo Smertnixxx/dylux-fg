5b417dd07b110d0983f44fb3854cafc7a4afc8eb
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:50:42 +0500

    Update dl-twitter.js

--- plugins/dl-twitter.js
+++ plugins/dl-twitter.js
@@ -1,18 +1,18 @@
 
 import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `📌 Ejemplo : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
+if (!args[0]) throw `📌 Пример : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
           m.react(rwait)    
           try {
           let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
           let te = ` 
-┌─⊷ *TWITTER DL*
-▢ Descripción: ${desc}
+┌─⊷ *ТВИТТЕР*
+▢ Описание: ${desc}
 └───────────`
 conn.sendFile(m.chat, HD, 'twitter.mp4', te, m)
 m.react(done)
 } catch (e) {
-  	m.reply(`✳️ verifica que el link sea de Twitter`)
+  	m.reply(`✳️ убедитесь, что ссылка взята из Твиттера`)
 	} 
 	
 }
