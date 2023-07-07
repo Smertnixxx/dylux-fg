fd280132c65840d96b8cf1c20e90302b2c769ef2
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 07:49:41 +0500

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,8 +1,8 @@
 
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
-  if (!args[0].match(/youtu/gi)) throw `❎ Verifica que el link de YouTube`
+  if (!args || !args[0]) throw `✳️ Пример :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
+  if (!args[0].match(/youtu/gi)) throw `❎ Убедитесь, что ссылка на YouTube`
    m.react(rwait)
  let chat = global.db.data.chats[m.chat]
   try {
@@ -13,14 +13,14 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 		const title = await yt.title
 		const size = await yt.audio[q].fileSizeH
 		conn.sendFile(m.chat, dl_url, title + '.mp3', `
- ≡  *FG YTDL*
+ ≡  *Меню*
   
-▢ *📌Titulo* : ${title}
-▢ *⚖️Tamaño* : ${size}
+▢ *📌Заголовок* : ${title}
+▢ *⚖️Размер* : ${size}
 `.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done)
         } catch {
-			await m.reply(`❎ Error: no se pudo descargar el audio`)
+			await m.reply(`❎ Ошибка: не удалось загрузить аудио`)
 } 
 
 }
