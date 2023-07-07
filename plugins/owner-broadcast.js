95d7900e6bede3bd296a97e9153e86127ef2fafb
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-07 09:10:14 +0500

    Update owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `✅ Transmision realizada *Total:* ${chats.length} chats`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*TRANSMISIÓN ┃ STAFF*\n_____________________\n ${teks} ` ), true).catch(_ => _)
-  m.reply('✅ Se transmitió a todos los chats :)')
+  conn.reply(m.chat, `✅ Осуществленная передача *TOtal:* ${chats.length} chats`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*ПЕРЕДАЧА ┃ Персонал*\n_____________________\n ${teks} ` ), true).catch(_ => _)
+  m.reply('✅Он транслировался во все чаты :)')
 }
 handler.help = ['tx']
 handler.tags = ['owner']
