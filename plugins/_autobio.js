124c8a988b08ac3ab82e04ece85aedfa5d64dea0
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-08 14:54:06 +0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 рабочее время ${muptime}\n\n ┃ 💎  By Baton`
+		let bio = `\n🟢 рабочее время ${muptime}\n\n ┃ 💎ULTIMATE|bot`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
