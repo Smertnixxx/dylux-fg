343c471674c26dbea18327a0387283eecb1455c8
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-08 13:01:56 +0500

    Update econ-work.js

--- plugins/econ-work.js
+++ plugins/econ-work.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 }
 handler.help = ['work']
 handler.tags = ['econ']
-handler.command = ['work', 'w', 'trabajar']
+handler.command = ['work', 'w', 'работать']
 
 export default handler
 
