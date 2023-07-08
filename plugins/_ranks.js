b4fe49902b21fd803647e67bc1392dacbfd44b45
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-09 00:40:02 +0500

    Update _ranks.js

--- plugins/_ranks.js
+++ plugins/_ranks.js
@@ -6,13 +6,13 @@ global.rpg = {
     if (isNaN(level)) return { name: '', level: '' }
     
     const role = [
-      { name: "Новичек", level: 0 }, { name: "Apprentice", level: 4 }, 
-      { name: "Опытный", level: 8 }, { name: "Magus", level: 12 }, 
-      { name: "Мастер", level: 16 }, { name: "Guardian", level: 20 }, 
-      { name: "Чемпион", level: 24 }, { name: "Hero", level: 28 }, 
-      { name: "Легенда", level: 32 }, { name: "Myth", level: 36 },
-      { name: "Волшебник", level: 48 }, { name: "Archmage", level: 52 }, 
-      { name: "Мудрец", level: 56 }, { name: "Divine", level: 60 }, 
+      { name: "Новичек", level: 0 }, { name: "начинающий", level: 4 }, 
+      { name: "Опытный", level: 8 }, { name: "Маг", level: 12 }, 
+      { name: "Мастер", level: 16 }, { name: "Опекун", level: 20 }, 
+      { name: "Чемпион", level: 24 }, { name: "Герой", level: 28 }, 
+      { name: "Легенда", level: 32 }, { name: "Мифический", level: 36 },
+      { name: "Волшебник", level: 48 }, { name: "Верховный маг", level: 52 }, 
+      { name: "Мудрец", level: 56 }, { name: "Святой", level: 60 }, 
       { name: "Всемогущий", level: 100 }
     ];
 
