cc3a90388ffd1a15dd9548532f5fd71a31ae3a6f
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-09 22:11:08 +0500

    Update gp-link.js

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
     if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Его можно использовать только в группах'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants no está definido :('
+    if (!('participants' in groupMetadata)) throw 'участники без определенного места жительства :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
     if (!me) throw '✳️ Я не в этой группе :('
     if (!me.admin) throw '✳️ Я не администратор'
