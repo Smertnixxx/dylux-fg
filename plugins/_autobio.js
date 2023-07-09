ca66282a8bc506c7a3d17a90fd5a4221223f20a2
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-10 00:31:37 +0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 рабочее время ${muptime}\n\n ┃ 💎ULTIMATE|bot`
+		let bio = `\n🌐 Бот активен уже ${muptime}\n\n ┃ ✨ULTIMATE|bot`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' День ️', h, ' Часов ', m, ' Минуты'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Дней ️', h, ' Часов ', m, ' Минут'].map(v => v.toString().padStart(2, 0)).join('')
 }
