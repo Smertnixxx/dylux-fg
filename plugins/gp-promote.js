e0a892e74d9b8a8084bd00feb7c61c23531f2d46
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-09 22:41:59 +0500

    Update gp-promote.js

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -7,7 +7,7 @@ var number = text.split`@`[1]
 var number = text
 }
 if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Использование команд \n *${usedPrefix + command}* @tag  (или ответьте на сообщение)`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Неправильный номер`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳❌ Неправильный номер`, m)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-m.reply(`✅ *%name* стал *Администратором*`)
+m.reply(`👑 Пользователь повышен до *Администратор*`)
 }}
 handler.help = ['promote']
 handler.tags = ['group']
