64f32a7f6cbc031262070909a753e516d667e07a
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-13 17:14:26 +0500

    Update main-blocklist.js

--- plugins/main-blocklist.js
+++ plugins/main-blocklist.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
 	})
 }
 
-handler.help = ['blocklist']
+handler.help = ['Заблокированные пользователи']
 handler.tags = ['main']
 handler.command = ['blocklist', 'listblock'] 
 
