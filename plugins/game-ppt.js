a0ba163e581ad37e16f53aac53bc3304a7b8e492
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-13 17:35:18 +0500

    Update game-ppt.js

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -2,7 +2,7 @@
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let poin = 300
-    let reseqv = `✳️ Выберете камень/Бумага/Ножницы\n\n🌿Пример : *${usedPrefix + command}* Бумага\n`
+    let reseqv = `✳️ Выберете Камень/Бумага/Ножницы\n\n🌿Пример : *${usedPrefix + command}* Бумага\n`
     if (!text) throw reseqv
     var astro = Math.random()
 
@@ -17,38 +17,38 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     if (text == astro) {
       global.db.data.users[m.sender].exp += 100
-        m.reply(`▢ *Сопереживать*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Точки (±)100 XP`)
+        m.reply(`▢ *Сопереживать*\n\n‣ Tú : ${text}\n‣ Бот : ${astro}\n\n🎁 Выйгрыш (±)100 XP`)
     } else if (text == 'Камень') {
         if (astro == 'Ножницы') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Выйграл* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Точки *+${poin} XP*`)
+            m.reply(`▢ *Выйграл* 🎊\n\n‣ Ты : ${text}\n‣ Бот : ${astro}\n\n🎁 Выйгрыш *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Потерял*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n Точки *-${poin} XP*`)
+            m.reply(`▢ *Потерял*\n\n‣ Ты : ${text}\n‣ Бот : ${astro}\n\n Выйгрыш *-${poin} XP*`)
         }
     } else if (text == 'Ножницы') {
         if (astro == 'Бумага') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Выйграл* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Точки *+${poin} XP*`)
+            m.reply(`▢ *Выйграл* 🎊\n\n‣ Tы : ${text}\n‣ Бот : ${astro}\n\n🎁 Выйгрыш *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Потерял*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\nPuntos *-${poin} XP*`)
+            m.reply(`▢ *Потерял*\n\n‣ Ты : ${text}\n‣ Бот : ${astro}\n\nВыйгрыш *-${poin} XP*`)
         }
     } else if (text == 'Бумага') {
         if (astro == 'Камень') {
             global.db.data.users[m.sender].exp += 300
-            m.reply(`▢ *Выйграл* 🎊\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\n🎁 Точки *+${poin} XP*`)
+            m.reply(`▢ *Выйграл* 🎊\n\n‣ Ты : ${text}\n‣ Бот : ${astro}\n\n🎁 Точки *+${poin} XP*`)
         } else {
           global.db.data.users[m.sender].exp -= 300
-            m.reply(`▢ *Потерял*\n\n‣ Tú : ${text}\n‣ DyLux : ${astro}\n\nТочки *-${poin} XP*`)
+            m.reply(`▢ *Потерял*\n\n‣ Ты : ${text}\n‣ Бот : ${astro}\n\nТочки *-${poin} XP*`)
         }
     } else {
         throw reseqv
     }
 }
-handler.help = ['ppt <piedra/papel/tijera>']
+handler.help = ['Суефа (Камень/Ножницы/Бумага']
 handler.tags = ['game']
-handler.command = ['ppt'] 
+handler.command = ['Суефа'] 
 handler.register = false
 
 export default handler
