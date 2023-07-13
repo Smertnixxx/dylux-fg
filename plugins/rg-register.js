7bbdeb508b75d83387546406a5b876e9232cac33
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-13 17:44:29 +0500

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -32,7 +32,7 @@ ${sn}
 handler.help = ['Регистрация'].map(v => v + ' <Имя.Лет>')
 handler.tags = ['rg']
 
-handler.command = ['verify', 'reg', 'register', 'Регистрация', 'registrar'] 
+handler.command = ['verify', 'reg', 'register', 'Регистрация', 'Рег', 'registrar'] 
 
 export default handler
 
