311272741e2d4ea557c54694bf53caa9304df182
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-07-13 17:49:27 +0500

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -12,7 +12,7 @@ let handler = async function (m, { conn, args, usedPrefix}) {
 handler.help = ['Отвязать учетную запись'] 
 handler.tags = ['rg']
 
-handler.command = ['Отвязать','hjj','unreg'] 
+handler.command = ['Делрег','hjj','unreg'] 
 handler.register = true
 
 export default handler
