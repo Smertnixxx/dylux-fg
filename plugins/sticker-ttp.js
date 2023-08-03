32eca3cbf732d41ac49ea98841b1bf817caa57ef
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-03 16:59:50 +0500

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -3,14 +3,14 @@ import { sticker } from '../lib/sticker.js'
 import fg from 'api-dylux'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     
-    if (!text) throw `✳️ Отправьте текст\n\n📌Пример *${usedPrefix + command}* Батон-бот`  
+    if (!text) throw `✳️ Отправьте текст\n\n📌Пример *${usedPrefix + command}* ULTIMATE-bot`  
     let color = '2FFF2E' //color
     let res = await fg.ttp(text, color) 
     let stiker = await sticker(null, res.result, global.packname, global.author)
     if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m, null, rpl)
     throw stiker.toString()
 }
-handler.help = ['ttp <text>']
+handler.help = ['ttp <текст>']
 handler.tags = ['sticker']
 handler.command = ['ttp']
 
