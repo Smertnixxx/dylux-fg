ebf70afae9f9aed585ddeddf0895b6995b2ab3be
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-03 22:58:57 +0500

    Update gp-resetLink.js

--- plugins/gp-resetLink.js
+++ plugins/gp-resetLink.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn }) => {
   let res = await conn.groupRevokeInvite(m.chat)
   m.reply('✅ Групповая ссылка была успешно сброшена\n\n📌 Новая ссылка:\nhttps://chat.whatsapp.com/' + res)
 }
-handler.help = ['resetlink']
+handler.help = ['убрать ссылку']
 handler.tags = ['group']
-handler.command = ['revoke', 'resetlink', 'anularlink'] 
+handler.command = ['revoke', 'resetlink', 'убрать ссылку'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
