e461fc67f37e7799915f1d703294387b2a811585
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-04 16:12:14 +0500

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -1,14 +1,14 @@
 
 import fg from 'api-dylux'
 let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Введите изображение, которое хотите найти \n\n📌 Пример: *${usedPrefix + command}* Billie Eilish`
+  if (!text) throw `✳️ Введите изображение, которое хотите найти \n\n📌 Пример: *${usedPrefix + command}* Илон Маск`
   let res = await fg.googleImage(text)
   conn.sendFile(m.chat, res.getRandom(), 'img.png', `
 ✅ Результат : *${text}*`.trim(), m)
 }
-handler.help = ['imagen']
+handler.help = ['Поиск']
 handler.tags = ['img']
-handler.command = /^(img|image|gimage|imagen)$/i
+handler.command = ['поиск']
 handler.diamond = true
 
 export default handler
