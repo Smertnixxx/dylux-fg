5032e32d7520fd05396c21c6f5c5d356e1f17391
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-04 14:47:35 +0500

    Update versions.js

--- plugins/versions.js
+++ plugins/versions.js
@@ -1,10 +1,8 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 if (!text) throw `✳️ Версия ULTIMATE PREMIUM 1.01
 if (text.includes('+')) throw  `✳️ Введите число все вместе без *+*`
-if (isNaN(text)) throw ' 📌 Вводите только цифры плюс код вашей страны без пробелов'
 let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
- 
+
       await conn.reply(text+'@s.whatsapp.net', `≡ *ПРИГЛАШЕНИЕ В ГРУППУ*\n\nОдин пользователь пригласил вас присоединиться к этой группе \n\n${link}`, m, {mentions: [m.sender]})
         m.reply(`✅ Пользователю была отправлена ссылка для приглашения`) 
 
