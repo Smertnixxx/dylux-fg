f7779430cdbf665f276ef4c0bc881f00dc25db64
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-04 21:48:38 +0500

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,7 +2,7 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Введите серийный номер*\nПроверьте свой серийный номер с помощью команды...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `✳️ *Введите серийный номер*\nПроверьте свой серийный номер с помощью команды\n\n*${usedPrefix}!номер*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw '⚠️ *Неправильный серийный номер*'
