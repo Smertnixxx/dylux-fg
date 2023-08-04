4917302668ab43927cf9563b1ca8e4614f181427
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-04 16:13:00 +0500

    Update img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,14 +1,14 @@
 import { pinterest } from '@bochilteam/scraper'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Какую картинку ты хочешь, чтобы я посмотрел?\n\N📌 Пример: ${usedPrefix + command} Lil Peep`
+  if (!text) throw `✳️ Какую картинку ты хочешь, чтобы я посмотрел?\n\N📌 Пример: ${usedPrefix + command} Владимир Путин`
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
 *▢  Pinterest:*  ${text}
 `.trim(), m)
 }
-handler.help = ['pinterest']
+handler.help = ['Пинтерест']
 handler.tags = ['img']
-handler.command = ['pinterest'] 
+handler.command = ['пинтерест'] 
 
 export default handler
