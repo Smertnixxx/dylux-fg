2adc6bfdc1d5ff0701bd1cb38719161cd49ebe20
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-04 21:47:41 +0500

    Update rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -3,11 +3,11 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 m.reply(`
-▢ *Номер серии* : ${sn}
+▢ *Серийный номер* : ${sn}
 `.trim())
 }
-handler.help = ['mysn']
+handler.help = ['Номер']
 handler.tags = ['rg']
-handler.command = ['nserie', 'sn', 'mysn'] 
+handler.command = ['nserie', 'sn', 'Номер'] 
 handler.register = true
 export default handler
