0014a9483c290b1bac2449bd7d352437a538170c
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-07 00:20:23 +0500

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -1,15 +1,11 @@
-//import db from '../lib/database.js'
-
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
-  if (text) {
-    global.db.data.chats[m.chat].sWelcome = text
-    m.reply('✅ Se configuro el mensaje de bienvenida')
-  } else throw `✳️ Ingrese el mensaje de Bienvenida\n\n@user (mención)\n@group (Nombre de grupo)\n@desc (description de grupo)`
+if (text) {
+global.db.data.chats[m.chat].sWelcome = text
+m.reply('*[❗] Сообщение приветствия настроено правильно для этой группы*')
+} else throw `*[❗] Введите желаемое добавленное приветственное сообщение, используйте:*\n*- @user (упоминание)*\n*- @group (название группы)*\n*- @desc (описание группы)*`
 }
-handler.help = ['setwelcome <text>']
-handler.tags = ['group']
-handler.command = ['setwelcome'] 
+handler.help = ['Приветсвие <Текст>']
+handler.tags = ['owner']
+handler.command = ['приветсвие'] 
 handler.admin = true
-handler.owner = true
-
 export default handler
