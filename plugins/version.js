a9c3a9416ec0edb39e3e02074ffb95dbb541a1d8
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-06 18:27:27 +0500

    Create version.js

---
+++ plugins/version.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+     m.reply(`привет`) null, {
+        mentions: users
+    })
+}
+
+handler.help = ['Версия']
+handler.tags = ['group']
+handler.command = ['tagall', 'версия
+handler.admin = true
+handler.group = true
+
+export default handler
