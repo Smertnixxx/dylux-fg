f336d8fd682ad19d8dea0cec07f63aae2c88963c
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-07 03:16:40 +0500

    Update version.js

--- plugins/version.js
+++ plugins/version.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🟢 *v1.01*`);
+          m.reply(`${ssd}🟢 *v2.00*`);
              conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('👊') 
             });
