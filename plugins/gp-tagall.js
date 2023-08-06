ce34d02ed4fca9a00bb20746336ea487b3719e4a
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-06 17:53:52 +0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`🖼️ Группа : *${groupMetadata.subject}*\n👥 Участники : *${participants.length}*${text ? `\n💬 Сообщение : *${text}\n` : *''}\n┌───⊷ *Участники*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ *ᑌᒪTIᗰᗩTE ┃ ᗷOT* ✪──', null, {
+    m.reply(`🖼️ Группа : *${groupMetadata.subject}*\n👥 Участники : *${participants.length}*${text ? `\n💬 Сообщение : ${text}\n` : ''}\n┌───⊷ *Участники*\n` + users.map(v => '👥 @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ *ᑌᒪTIᗰᗩTE ┃ ᗷOT* ✪──', null, {
         mentions: users
     })
 }
