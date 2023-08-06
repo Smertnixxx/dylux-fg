84f5abf409423c7c08e0fb49536e07876e027fe5
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-06 18:34:25 +0500

    Update version.js

--- plugins/version.js
+++ plugins/version.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
- m.reply(`${ssd}🟢 *Скорость* : ${latensi.toFixed(4)} _ms_`);
+ m.reply(`${ssd}🟢 *Скорость* :`);
     
     })
 }
