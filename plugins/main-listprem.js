e296267d4f87831e96358eda1cb77043385d7e7d
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-08 02:02:47 +0500

    Update main-listprem.js

--- plugins/main-listprem.js
+++ plugins/main-listprem.js
@@ -1,12 +1,12 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid) 
-let teks = `▢ *ПРЕМИУМ ПОЛЬЗОВАТЕЛИ*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
+let teks = `🫅 *ПРЕМИУМ ПОЛЬЗОВАТЕЛИ*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 m.reply(teks, null, {mentions: conn.parseMention(teks)})
 
 }
-handler.help = ['Премлист']
+handler.help = ['ПремиумЛист']
 handler.tags = ['main']
-handler.command = ['listprem', 'premlist', 'премлист', 'listpremium'] 
+handler.command = ['listprem', 'premlist', 'премиумлист', 'listpremium'] 
 
 export default handler
