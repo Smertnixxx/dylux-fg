f3649c6ae4e5111b00c9e066420ba3bdbef1c14d
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-08 03:11:43 +0500

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,27 +1,42 @@
 import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
-let handler = async(m, { conn, usedPrefix, participants, isPrems }) => {
-let pp = 'https://i.imgur.com/WHjtUae.jpg'
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`
-try {
-pp = await conn.profilePictureUrl(who)
-} catch (e) {
-} finally {
-let { name, limit, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix, command}) => {
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let user = global.db.data.users[who]
+let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+let { min, xp, max } = xpRange(user.level, global.multiplier)
 let username = conn.getName(who)
-let prem = global.prems.includes(who.split `@` [0])
+let math = max - xp
+let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
-let str = `*🏷️Имя:* ${username} ${registered ? '(' + name + ') ': ''}
-*📱Номер:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*🔗Ссылка:* wa.me/${who.split`@`[0]}${registered ? '\n*Возраст:* ' + age + ' Лет' : ''}
-*📇Зарегистрированный:* ${registered ? 'Да': 'Нет'}
-*⭐Премиум:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Да' : 'Нет') || ''}
-${sn}`
-//conn.sendButton(m.chat, str, author, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)
-}}
-handler.help = ['Профиль [@Пользователь]']
+
+let str = `
+┌───「 *PERFIL* 」
+▢ *🔖 Nombres:* 
+   • ${username} ${registered ? '\n   • ' + name + ' ': ''}
+   • @${who.replace(/@.+/, '')}
+▢ *📱Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? '\n▢ *🎈Edad*: ' + age + ' años' : ''}
+▢ *⚠️Advertencias:* ${warn}/${maxwarn}
+▢ *💎 Diamantes :* ${diamond}
+▢ *🆙 Nivel* : ${level}
+▢ *⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de nivel`}
+▢ *🏆Rango:* ${role}
+▢ *📇 Registrado :* ${registered ? 'Si': 'No'}
+▢ *⭐ Premium* : ${prem ? 'Si' : 'No'}
+└──────────────`
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
+    m.react(done)
+
+}
+handler.help = ['perfil']
 handler.tags = ['group']
-handler.command = ['профиль']
+handler.command = ['profile', 'perfil'] 
+
 export default handler
