9747b8c77d913b54c5fd10ab3efa8a2ba676e6b9
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-08 02:05:19 +0500

    Update main-listprem.js

--- plugins/main-listprem.js
+++ plugins/main-listprem.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid) 
-let teks = `🫅 *ПРЕМИУМ ПОЛЬЗОВАТЕЛИ*\n◄♦◊━━━━━━━━━━━━━━━━◊♦►\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
+let teks = `🫅 *ПРЕМИУМ ПОЛЬЗОВАТЕЛИ*\n◄♦◊━━━━━━━━━━━━━━━━◊♦►\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n◄♦◊━━━━━━━━━━━━━━━━◊♦►\n`
 m.reply(teks, null, {mentions: conn.parseMention(teks)})
 
 }
