76185abf6e07002e040db64b9582385138cbe105
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-08 01:58:59 +0500

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
   if (!text) throw `✳️ Введите изображение, которое хотите найти \n\n📌 Пример: *${usedPrefix + command}* Илон Маск`
   let res = await fg.googleImage(text)
   conn.sendFile(m.chat, res.getRandom(), 'img.png', `
-✅ Результат : *${text}*`.trim(), m)
+✅ Результат : ${text}`.trim(), m)
 }
 handler.help = ['Поиск']
 handler.tags = ['img']
