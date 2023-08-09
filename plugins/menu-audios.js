4fbcdcbde089f7888bba5a7fb67a577c46938ae2
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-09 18:09:24 +0500

    Create menu-audios.js

---
+++ plugins/menu-audios.js
@@ -0,0 +1,164 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+try {
+let pp = imagen4
+//let vn = './media/menu.mp3'
+let img = await(await fetch('https://i.imgur.com/JP52fdP.jpg')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
+║    ◉— *𝕌𝕃𝕋𝕀𝕄𝔸𝕋𝔼 - ℙℝ𝔼𝕄𝕀𝕌𝕄* —◉
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+║➤ *Привет, ${taguser}*
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+
+┏━━━━━━━━━━━━━━━━┓
+┃ *<МЕНЮ РАЗВЛЕЧЕНИЙ/>*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣*- Напишите следующие слова или фразы без каких-либо префиксов (#, /, *, .)*
+┣ ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
+┣ ඬ⃟🔊 _Te diagnostico con gay_
+┣ ඬ⃟🔊 _A nadie le importa_
+┣ ඬ⃟🔊 _Fiesta del admin_
+┣ ඬ⃟🔊 _Fiesta del administrador_ 
+┣ ඬ⃟🔊 _Vivan los novios_
+┣ ඬ⃟🔊 _Feliz cumpleaños_
+┣ ඬ⃟🔊 _Noche de paz_
+┣ ඬ⃟🔊 _Buenos dias_
+┣ ඬ⃟🔊 _Buenos tardes_
+┣ ඬ⃟🔊 _Buenos noches_
+┣ ඬ⃟🔊 _Audio hentai_
+┣ ඬ⃟🔊 _Chica lgante_
+┣ ඬ⃟🔊 _Feliz navidad_
+┣ ඬ⃟🔊 _Vete a la vrg_
+┣ ඬ⃟🔊 _Pasa pack Bot_
+┣ ඬ⃟🔊 _Atencion grupo_
+┣ ඬ⃟🔊 _Marica quien_
+┣ ඬ⃟🔊 _Murio el grupo_
+┣ ඬ⃟🔊 _Oh me vengo_
+┣ ඬ⃟🔊 _tio que rico_
+┣ ඬ⃟🔊 _Viernes_
+┣ ඬ⃟🔊 _Baneado_
+┣ ඬ⃟🔊 _Sexo_
+┣ ඬ⃟🔊 _Hola_
+┣ ඬ⃟🔊 _Un pato_
+┣ ඬ⃟🔊 _Nyanpasu_
+┣ ඬ⃟🔊 _Te amo_
+┣ ඬ⃟🔊 _Yamete_
+┣ ඬ⃟🔊 _Bañate_
+┣ ඬ⃟🔊 _Es puto_
+┣ ඬ⃟🔊 _La biblia_
+┣ ඬ⃟🔊 _Onichan_
+┣ ඬ⃟🔊 _Mierda de Bot_
+┣ ඬ⃟🔊 _Siuuu_
+┣ ඬ⃟🔊 _Epico_
+┣ ඬ⃟🔊 _Shitpost_
+┣ ඬ⃟🔊 _Rawr_
+┣ ඬ⃟🔊 _UwU_
+┣ ඬ⃟🔊 _:c_
+┣ ඬ⃟🔊 _a_
+┗━━━━━━━━━━━━━━━━┛`.trim()
+if (m.isGroup) {
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })  
+} else {    
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })}
+} catch {
+conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] В меню есть ошибка, и отправить ее не удалось, сообщите об этом при запуске бота*', m)
+}}
+handler.command = /^(menu2|audios|меню2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i
+handler.exp = 50
+handler.fail = null
+export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+
+//const { levelling } = '../lib/levelling.js'
+//import PhoneNumber from 'awesome-phonenumber'
+//let handler = async (m, { conn, usedPrefix }) => {
+//let pp = imagen4
+//try {
+//} catch (e) {
+//} finally {
+//let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+//let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+//let str = `
+//*ミ💖 𝙷𝙾𝙻𝙰 ${taguser} 💖彡*
+/*<𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒/>*
+*- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)*
+
+° ඬ⃟🔊 _Quien es tu sempai botsito 7w7_
+° ඬ⃟🔊 _Te diagnostico con gay_
+° ඬ⃟🔊 _A nadie le importa_
+° ඬ⃟🔊 _Fiesta del admin_
+° ඬ⃟🔊 _Fiesta del administrador_ 
+° ඬ⃟🔊 _Vivan los novios_
+° ඬ⃟🔊 _Feliz cumpleaños_
+° ඬ⃟🔊 _Noche de paz_
+° ඬ⃟🔊 _Buenos dias_
+° ඬ⃟🔊 _Buenos tardes_
+° ඬ⃟🔊 _Buenos noches_
+° ඬ⃟🔊 _Audio hentai_
+° ඬ⃟🔊 _Chica lgante_
+° ඬ⃟🔊 _Feliz navidad_
+° ඬ⃟🔊 _Vete a la vrg_
+° ඬ⃟🔊 _Pasa pack Bot_
+° ඬ⃟🔊 _Atencion grupo_
+° ඬ⃟🔊 _Marica quien_
+° ඬ⃟🔊 _Murio el grupo_
+° ඬ⃟🔊 _Oh me vengo_
+° ඬ⃟🔊 _tio que rico_
+° ඬ⃟🔊 _Viernes_
+° ඬ⃟🔊 _Baneado_
+° ඬ⃟🔊 _Sexo_
+° ඬ⃟🔊 _Hola_
+° ඬ⃟🔊 _Un pato_
+° ඬ⃟🔊 _Nyanpasu_
+° ඬ⃟🔊 _Te amo_
+° ඬ⃟🔊 _Yamete_
+° ඬ⃟🔊 _Bañate_
+° ඬ⃟🔊 _Es puto_
+° ඬ⃟🔊 _La biblia_
+° ඬ⃟🔊 _Onichan_
+° ඬ⃟🔊 _Mierda de Bot_
+° ඬ⃟🔊 _Siuuu_
+° ඬ⃟🔊 _Epico_
+° ඬ⃟🔊 _Shitpost_
+° ඬ⃟🔊 _Rawr_
+° ඬ⃟🔊 _UwU_
+° ඬ⃟🔊 _:c_
+° ඬ⃟🔊 _a_
+`.trim()
+if (m.isGroup) {
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: m })    
+} else {    
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })}
+//conn.sendButton(m.chat, str, wm, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m, { mentions: [m.sender] })
+}}
+handler.help = ['menu2', 'help2', '?2', 'menuaudios']
+handler.tags = ['main']
+handler.command = /^(menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i
+handler.fail = null
+export default  handler*/
