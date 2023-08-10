6887d5d2266acb0b1b2e0d90d5ef6edc27f510d5
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-10 17:08:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ULTIMATE -  SMERTNIX', {
+say('DyLux -  FG98', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @smertnix._`, {
+say(`'${name}' By @fg98._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
