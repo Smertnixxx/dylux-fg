da510cbee7cbb28c87936ca4c2eec2aeb80fe491
Smertnixxx <138637755+Smertnixxx@users.noreply.github.com>
2023-08-10 18:43:52 +0500

    Update _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -19,83 +19,82 @@ if (!m.messageStubType || !m.isGroup) return !0
    let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
    
    if (chat.detect2 && m.messageStubType == 29) {
-      let txt1 = `*Участник был повышен до Администратора.*\n\n`
-         txt1 += `*◦  Группа:* ${groupName}\n`
-         txt1 += `*◦  Новый администратор:* @${m.messageStubParameters[0].split`@`[0]}\n`
-         txt1 += `*◦  Выполненный:* @${m.sender.split`@`[0]}`   
-      await conn.sendMessage(m.chat, { image: img, caption: txt1, mentions: mentionsString }, { quoted: fkontak2 }) 
+      let txt1 = `*Recientemente se ha promovido un miembro a administrador.*\n\n`
+         txt1 += `*◦  Grupo:* ${groupName}\n`
+         txt1 += `*◦  Nuevo admin:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt1 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
+await conn.sendMessage(m.chat, { image: img, caption: txt1, mentions: mentionsString }, { quoted: fkontak2 })     
 }
     
    if (chat.detect2 && m.messageStubType == 30) {
-      let txt2 = `*Администратор был понижен в должности до участника.*\n\n`
-         txt2 += `*◦  Группа:* ${groupName}\n`
-         txt2 += `*◦  Снятый админ:* @${m.messageStubParameters[0].split`@`[0]}\n`
-         txt2 += `*◦  Понизил:* @${m.sender.split`@`[0]}`
-      await conn.sendMessage(m.chat, { image: img, caption: txt2, mentions: mentionsString }, { quoted: fkontak2 })
+      let txt2 = `*Recientemente se ha degradado un administrador a miembro.*\n\n`
+         txt2 += `*◦  Grupo:* ${groupName}\n`
+         txt2 += `*◦  Se quitó a:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt2 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
+await conn.sendMessage(m.chat, { image: img, caption: txt2, mentions: mentionsString }, { quoted: fkontak2 })
 }
 
-    if (chat.detect2 && m.messageStubType == 27) {
-      let txt3 = `*К группе присоединился новый участник.*\n\n`
-         txt3 += `*◦  Представьтесь новичок Имя, Фото, Голосовое сообщение, Город*\n`
+   if (chat.detect2 && m.messageStubType == 27) {
+      let txt3 = `*Recientemente se ha incorporado al grupo un nuevo miembro.*\n\n`
+         txt3 += `*◦  Grupo:* ${groupName}\n`
      if (!m.sender.endsWith('@g.us')) {
-         txt3 += `*◦  Новый участник:* @${m.messageStubParameters[0].split`@`[0]}\n`
-         txt3 += `*◦  Одобрил(а) заявку на вступление:* @${m.sender.split`@`[0]}`
+         txt3 += `*◦  Se añadió a:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt3 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
    } else {
-         txt3 += `*◦  Было добавлено:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt3 += `*◦  Se añadió:* @${m.messageStubParameters[0].split`@`[0]}\n`
    }
-       await conn.sendMessage(m.chat, { image: img, caption: txt3, mentions: mentionsContentM }, { quoted: fkontak2 })
+await conn.sendMessage(m.chat, { image: img, caption: txt3, mentions: mentionsContentM }, { quoted: fkontak2 })
 }
     
    if (chat.detect2 && m.messageStubType == 28) {
-      let txt4 = `*Участник был удален из группы.*\n\n`
-         txt4 += `*◦  Группа:* ${groupName}\n` 
+      let txt4 = `*Recientemente se ha eliminado un miembro del grupo.*\n\n`
+         txt4 += `*◦  Grupo:* ${groupName}\n` 
      if (!m.sender.endsWith('@g.us')) {        
-         txt4 += `*◦  Был удален(а):* @${m.messageStubParameters[0].split`@`[0]}\n`
-         txt4 += `*◦  Удалил:* @${m.sender.split`@`[0]}`
+         txt4 += `*◦  Se eliminó a:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt4 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
    } else {
-         txt4 += `*◦  Был удален(а):* @${m.messageStubParameters[0].split`@`[0]}\n`   
-   }   
-      await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt4, mentions: mentionsContentM }, { quoted: fkontak2 })
+         txt4 += `*◦  Se eliminó a:* @${m.messageStubParameters[0].split`@`[0]}\n`   
+   }       
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt4, mentions: mentionsContentM }, { quoted: fkontak2 })
 }
        
    if (chat.detect2 && m.messageStubType == 32) {
       let ax 
     if (m.messageStubParameters[0] === m.sender) {
-    ax = 'к сожалению'    
+    ax = 'salido'    
     } else {
-    ax = 'удаленный'    
+    ax = 'eliminado'    
     }
-      let txt5 = `*Участник ${ax} вышел из группы.*\n\n`
-         txt5 += `*◦  Группа:* ${groupName}\n`
-     if (ax === 'удаленный') {       
-         txt5 += `*◦  Уходи и больше не возвращайся предатель!*\n`
-         txt5 += `*◦  Выполненый:* @${m.sender.split`@`[0]}`
+      let txt5 = `*Recientemente se ha ${ax} un miembro del grupo.*\n\n`
+         txt5 += `*◦  Grupo:* ${groupName}\n`
+     if (ax === 'eliminado') {       
+         txt5 += `*◦  Se eliminoó a:* @${m.messageStubParameters[0].split`@`[0]}\n`
+         txt5 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
    } else {
-         txt5 += `*◦  Предатель:* @${m.messageStubParameters[0].split`@`[0]}\n`   
+         txt5 += `*◦  Se salió:* @${m.messageStubParameters[0].split`@`[0]}\n`   
    }            
-      await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt5, mentions: mentionsContentM }, { quoted: fkontak2 })
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt5, mentions: mentionsContentM }, { quoted: fkontak2 })
 }    
     
    if (chat.detect2 && m.messageStubType == 26) {
       let accion  
     if (m.messageStubParameters[0].split`@`[0] === 'on') {
-      accion = 'закрыта'    
+      accion = 'cerrado'    
       } else {
-      accion = 'открыта'   
+      accion = 'abierto'   
       }
-      let txt6 = `*Были изменены настройки группы.*\n\n`
-        txt6 += `*◦  Группа:* ${groupName}\n`
-        txt6 += `*◦  Группа была:* ${'```' + accion + '```'}\n`
-        txt6 += `*◦  Изменил:* @${m.sender.split`@`[0]}`
-      await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt6, mentions: mentionsContentM }, { quoted: fkontak2 })
+      let txt6 = `*Recientemente se han modificado los ajustes del grupo.*\n\n`
+        txt6 += `*◦  Grupo:* ${groupName}\n`
+        txt6 += `*◦  El grupo se ha:* ${'```' + accion + '```'}\n`
+        txt6 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt6, mentions: mentionsContentM }, { quoted: fkontak2 })
 }
     
    if (chat.detect2 && m.messageStubType == 21) {
-      let txt7 = `*Название группы было изменено.*\n\n`
-         txt7 += `*◦  Новое имя:* ${'```' + groupName + '```'}\n`
-         txt7 += `*◦  Изменил:* @${m.sender.split`@`[0]}`
-      await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt7, mentions: mentionsContentM }, { quoted: fkontak2 })
- 
+      let txt7 = `*Recientemente se ha cambiado el nombre del grupo.*\n\n`
+         txt7 += `*◦  Nuevo nombre:* ${'```' + groupName + '```'}\n`
+         txt7 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: txt7, mentions: mentionsContentM }, { quoted: fkontak2 })
 }
 
 } /* Cierre del comando */
