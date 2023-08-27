1081ec95e57b514f702c12dbe7cc3e05375f686c
Wildovsky <138637755+Smertnixxx@users.noreply.github.com>
2023-08-27 16:08:37 +0500

    Update game-math_answer.js

--- plugins/game-math_answer.js
+++ plugins/game-math_answer.js
@@ -4,7 +4,7 @@ let handler = m => m
 handler.before = async function (m) {
     if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^▢ Сколько это/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^❓ Сколько это/i.test(m.quoted.text)) return !0
     this.math = this.math ? this.math : {}
     if (!(id in this.math)) return this.reply(m.chat, 'Игра окончена', m)
     if (m.quoted.id == this.math[id][0].id) {
